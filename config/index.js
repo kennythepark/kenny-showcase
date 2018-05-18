9820d36d7d6407eed6c7c6b069838633946764fe
Kenny Park <parkubc@gmail.com>
2018-05-18 02:58:57 -0700

    fix asset path issue

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/kenny-showcase',
     proxyTable: {},
 
     // Various Dev Server settings
